fab6e3acea793fd7c5e9ecdcb442b59a8adf3e81
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-09-14 23:26:50 +0530

    Add footer section with basic layout and styling

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="container text-center py-8 text-gray-500">
+      Future Intern &copy; 2024 - All rights reserved
+    </footer>
+  );
+}

ee1894dcc933bd7d4cdcbb9e9185c127716818cf
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-09-15 21:56:07 +0530

    feat: added authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
 import Homee from "./components/Hero";
 import Jobs from "./components/Jobs";
-export default function Home() {
+import Link from "next/link";
+import {
+  getSignInUrl,
+  getSignUpUrl,
+  getUser,
+  signOut,
+} from "@workos-inc/authkit-nextjs";
+
+export default async function Home() {
+  // Retrieves the user from the session or returns `null` if no user is signed in
+  const { user } = await getUser();
+
+  // Get the URL to redirect the user to AuthKit to sign in
+  const signInUrl = await getSignInUrl();
+
+  // Get the URL to redirect the user to AuthKit to sign up
+  const signUpUrl = await getSignUpUrl();
+
   return (
     <>
       <Homee />

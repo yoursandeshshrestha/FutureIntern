a4f30be50a5d1527bd48d936124ceaa2449f4c50
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-09-15 21:57:29 +0530

    fix: added conditional rendering between login/logout in header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,17 +1,45 @@
 import Link from "next/link";
+import {
+  getSignInUrl,
+  getSignUpUrl,
+  getUser,
+  signOut,
+} from "@workos-inc/authkit-nextjs";
 
 export default async function Header() {
+  const { user } = await getUser();
+  const signInUrl = await getSignInUrl();
   return (
     <header>
       <div className="flex items-center justify-between mx-auto">
         <Link href={"/"} className="font-semibold text-3xl ">
           Future Intern
         </Link>
-        <nav className="flex gap-2 *:px-4 *:rounded-md *:py-2">
-          <Link className="bg-gray-200" href={"/login"}>
-            Login
-          </Link>
-          <Link className="bg-blue-600 text-white" href={"/new-listing"}>
+        <nav className="flex gap-2">
+          {!user && (
+            <Link className="bg-gray-200 px-4 rounded-md py-2" href={signInUrl}>
+              Login
+            </Link>
+          )}
+          {user && (
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              <button
+                type="submit"
+                className="bg-gray-200 px-4 rounded-md py-2"
+              >
+                Logout
+              </button>
+            </form>
+          )}
+          <Link
+            className="bg-blue-600 text-white px-4 rounded-md py-2"
+            href={"/new-listing"}
+          >
             Post a intership
           </Link>
         </nav>
